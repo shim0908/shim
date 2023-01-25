a1d744098234eeae5d3ec066c1fad928e2fa16bc
shim0908 <119276308+shim0908@users.noreply.github.com>
2023-01-25 18:51:16 +0900

    Update shim07.py

--- shim07.py
+++ shim07.py
@@ -3,6 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
+from matplotlib import font_manager, rc
+font = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font)
+
+
 st.title('dizzy :face_with_spiral_eyes:')
 
 st.sidebar.success("Select a demo above.")
@@ -26,23 +31,23 @@ fig, ax = plt.subplots(2,2, figsize=(12,8))
 plt.subplot(221)
 plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('America Rate')
+plt.title('미국금리')
 
 
 plt.subplot(222)
 plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('Korea Rate')
+plt.title('한국금리')
 
 plt.subplot(223)
 plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('KOSPI')
+plt.title('코스피')
 
 plt.subplot(224)
 plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('House Price')
+plt.title('')
 
 st.pyplot(fig)
 st.dataframe(money)
