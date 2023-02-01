ed57a6a11ad0c75fa7dd0cad501bdfde1a2e9267
shim0908 <119276308+shim0908@users.noreply.github.com>
2023-02-01 17:23:51 +0900

    Update shim07.py

--- shim07.py
+++ shim07.py
@@ -83,7 +83,7 @@ def bar_chart():
     plt.bar(  x,  y,  color= colors ) 
 
     for   num ,   v    in   enumerate( y ):
-        plt.text (  num -0.4  ,   v + 0.01 ,  v   )
+        plt.text (  num -0.4  ,   v + 0.05 ,  v   )
 
     plt.title( "year korea baseball winrate data", position=(0.5,1.1))
     st.pyplot(fig)
