45b12d3369cbcebbafd2ff751b926c5fdc2c3514
shim0908 <119276308+shim0908@users.noreply.github.com>
2023-02-01 19:08:39 +0900

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-  image = Image.open('img.jpg')
+image = Image.open('img.jpg')
 #st.title("EDA Package")
 
 def  plotting_demo():
